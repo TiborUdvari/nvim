6d342339672efeefbb62f39a0c1d2f807528f9a3
Tibor Udvari <tibor.udvari@icloud.com>
2025-12-27 22:10:41 +0100

    some changes

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,14 +1,29 @@
 require("conform").setup({
   formatters_by_ft = {
-    astro = { "prettierd", "prettier", stop_after_first = true },
+    astro = { "prettier", "prettierd", stop_after_first = true },
+    mdx = { "prettier", "prettierd", stop_after_first = true },
     yaml = { "yamlfix" },
     lua = { "stylua" },
     python = { "isort", "black" },
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-    html = { "prettierd", stop_after_first = true }
+    javascript = { "prettier", "prettierd", stop_after_first = true },
+    typescript = { "prettier", "prettierd", stop_after_first = true },
+    html = { "prettierd", stop_after_first = true },
+    typescriptreact = { "prettier", "prettierd", stop_after_first = true }
   },
-  async = true,
-  lsp_fallback = true,
+  formatters = {
+    prettier = {
+      command = "npx",
+      args = { "prettier", "--stdin-filepath", "$FILENAME" },
+      cwd = require("conform.util").root_file({
+        ".prettierrc",
+      }),
+    },
+  },
+  default_format_opts = {
+    async = true,
+    lsp_fallback = true,
+    timeout_ms=3000,
+  },
 })
 
 vim.api.nvim_set_keymap('n', '<leader>cf', '<cmd>lua require("conform").format()<CR>', { noremap = true, silent = true })
